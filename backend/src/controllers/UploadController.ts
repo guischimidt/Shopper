1f4e1495c23cfbba10b265d72c9f13adc6dfe950
Guilherme Schimidt <guigoschimidt@gmail.com>
2023-09-06 14:36:08 -0300

    refactor: move validations to CssProcessingUseCase

--- backend/src/controllers/UploadController.ts
+++ backend/src/controllers/UploadController.ts
@@ -1,6 +1,5 @@
 import { type Request, type Response } from 'express'
 import CsvParserService from '../services/CsvParserService'
-import CsvValidationUseCase from '../usecases/CsvValidationUseCase'
 import CsvProcessingUseCase from '../usecases/CsvProcessingUseCase'
 
 interface MulterRequest extends Request {
@@ -18,16 +17,9 @@ class UploadController {
 
     try {
       const csvData = await CsvParserService.parseCsv(req.file.buffer)
-      const validationErrors = await CsvValidationUseCase.validateCsvRows(csvData)
-
-      if (validationErrors.length > 0) {
-        res.status(400).json({ errors: validationErrors })
-        return
-      }
-
       const processedData = await CsvProcessingUseCase.processCsv(csvData)
 
-      res.status(200).json({ message: 'Arquivo CSV processado com sucesso.', data: processedData })
+      res.status(200).json({ message: 'Arquivo CSV processado com sucesso.', processedData })
     } catch (error) {
       console.error('Erro ao processar o arquivo CSV:', error)
       res.status(500).json({ error: 'Erro ao processar o arquivo CSV.' })
--- backend/src/usecases/CsvProcessingUseCase.ts
+++ backend/src/usecases/CsvProcessingUseCase.ts
@@ -2,14 +2,15 @@ import ProductRepository from '../repositories/ProductRepository'
 
 interface CsvRow {
   product_code: string
-  new_price: string
+  new_price: number
 }
 
 interface ProcessedData {
   code: string | number
   name: string
   sales_price: number
-  new_price: string
+  new_price: number
+  errors: string[]
 }
 
 class CsvProcessingUseCase {
@@ -18,13 +19,25 @@ class CsvProcessingUseCase {
     const products = await ProductRepository.findByCodes(productCodes)
 
     const processedData = rows.map((row) => {
+      const errors: string[] = []
       const product = products.find((p) => p.code === Number(row.product_code))
+
+      if (!row.product_code || !row.new_price) {
+        errors.push('Código ou preço não informados')
+      } else if (isNaN(Number(row.new_price))) {
+        errors.push('Preço precisa ser um número')
+      } else if (!product) {
+        errors.push('Produto não encontrado')
+      } else if (row.new_price < product.cost_price) {
+        errors.push('O novo preço não pode ser menor que o preço de custo')
+      }
 
       return {
         code: product ? product.code : row.product_code,
         name: product ? product.name : 'Produto não encontrado',
         sales_price: product ? product.sales_price : 0,
-        new_price: row.new_price
+        new_price: row.new_price,
+        errors
       }
     })
 
