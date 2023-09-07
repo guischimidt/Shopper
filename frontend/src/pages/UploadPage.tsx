7567d5148a903efaa6c3054cb7aa63a36059af7d
Guilherme Schimidt <guigoschimidt@gmail.com>
2023-09-07 19:07:45 -0300

    refactor: move Atualizar to UploadPage

--- frontend/src/pages/UploadPage.tsx
+++ frontend/src/pages/UploadPage.tsx
@@ -7,9 +7,11 @@ import DataTable from '../components/DataTable';
 import UploadCSVUseCase from '../domain/use-cases/UploadCSVUseCase';
 import CSVRepository from '../domain/repositories/CSVRepository';
 import { DataItem } from '../interfaces/interfaces';
+import Button from '@mui/material/Button';
 
 function UploadPage() {
     const [apiData, setApiData] = useState<DataItem[]>([]);
+    const [fileUploaded, setFileUploaded] = useState(false);
 
     const handleFileUpload = async (file: File) => {
         try {
@@ -20,27 +22,43 @@ function UploadPage() {
             const data = await useCase.execute(file);
 
             setApiData(data.processedData);
+            setFileUploaded(true);
         } catch (error) {
             console.error('Erro ao enviar o arquivo para a API:', error);
         }
     };
 
-    return (<Container>
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                mt: 4,
-            }}
-        >
-            <Typography variant="h4" component="h1" gutterBottom>
-                Upload de Arquivo CSV
-            </Typography>
-            <UploadComponent onFileUpload={handleFileUpload} />
-            {apiData.length > 0 && <DataTable data={apiData} />}
-        </Box>
-    </Container>
+    const hasErrors = apiData.some(item => item.errors.length > 0);
+
+    return (
+        <Container>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    mt: 4,
+                }}
+            >
+                <Typography variant="h4" component="h1" gutterBottom>
+                    Upload de Arquivo CSV
+                </Typography>
+                <UploadComponent onFileUpload={handleFileUpload} />
+                {apiData.length > 0 && <DataTable data={apiData} />}
+                {fileUploaded && !hasErrors && (
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => {
+                            // Coloque aqui a lógica para atualizar os dados
+                            // Esta função será chamada apenas se não houver erros e um arquivo foi enviado
+                        }}
+                    >
+                        Atualizar
+                    </Button>
+                )}
+            </Box>
+        </Container>
     );
 }
 
