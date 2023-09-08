6824fdcad897fa91b6e4c8be66427aee0308a2f0
Guilherme Schimidt <guigoschimidt@gmail.com>
2023-09-08 15:48:36 -0300

    refactor: use dotenv

--- backend/src/infra/mysql.ts
+++ backend/src/infra/mysql.ts
@@ -1,11 +1,14 @@
 import { Sequelize } from 'sequelize'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const sequelize = new Sequelize({
   dialect: 'mysql',
-  host: 'localhost',
-  username: 'root',
-  password: '',
-  database: 'shopper'
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 })
 
 export default sequelize
