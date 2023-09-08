a0ba5228c9c7431c7784fee20dd5d2903aad0c5d
Guilherme Schimidt <guigoschimidt@gmail.com>
2023-09-08 17:25:21 -0300

    fix: fix import .env with VITE

--- frontend/src/infrastructure/api/ApiService.tsx
+++ frontend/src/infrastructure/api/ApiService.tsx
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from 'axios';
 import { UpdateItem } from '../../interfaces/interfaces';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 class ApiService {
     async uploadCSVFile(formData: FormData): Promise<AxiosResponse<any>> {
