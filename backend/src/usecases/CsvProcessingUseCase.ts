89c8d12656ef0506fc61d35ca5f2406d6f787745
Guilherme Schimidt <guigoschimidt@gmail.com>
2023-09-08 01:27:26 -0300

    fix: fix when product not found

--- backend/src/usecases/CsvProcessingUseCase.ts
+++ backend/src/usecases/CsvProcessingUseCase.ts
@@ -21,19 +21,22 @@ class CsvProcessingUseCase {
     const processedData = rows.map((row) => {
       const errors: string[] = []
       const product = products.find((p) => p.code === Number(row.product_code))
-      const percentageThreshold = 0.1
-      const priceDifference = Math.abs(Number(row.new_price) - Number(product.sales_price))
-
       if (!row.product_code || !row.new_price) {
         errors.push('Código ou preço não informados')
       } else if (isNaN(Number(row.new_price))) {
         errors.push('Preço precisa ser um número')
       } else if (!product) {
         errors.push('Produto não encontrado')
-      } else if (Number(row.new_price) < Number(product.cost_price)) {
-        errors.push('O novo preço não pode ser menor que o preço de custo')
-      } else if (Number(priceDifference) / Number(product.sales_price) > percentageThreshold) {
-        errors.push('O novo preço deve ser no máximo 10% maior ou menor que o preço de venda atual')
+      } else {
+        if (Number(row.new_price) < Number(product.cost_price)) {
+          errors.push('O novo preço não pode ser menor que o preço de custo')
+        }
+        const percentageThreshold = 0.1
+        const priceDifference = Math.abs(Number(row.new_price) - Number(product.sales_price))
+
+        if (Number(priceDifference) / Number(product.sales_price) > percentageThreshold) {
+          errors.push('O novo preço deve ser no máximo 10% maior ou menor que o preço de venda atual')
+        }
       }
 
       return {
